277f9684619e632b206ab5154dd2475f5819410e
msadhimb <adhimbaqy72@gmail.com>
2024-04-07 23:34:29 +0700

    Feat: Create Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-full justify-center items-center h-screen">
+        <p className="text-2xl font-bold">
+          Thid Project is created using Vite + React + Tailwind CSS
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
