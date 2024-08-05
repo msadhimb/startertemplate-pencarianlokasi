4121a9ec8f39ee190ff2b1bf96859d81d057e70f
Salman Adhim <adhimbaqy72@gmail.com>
2024-08-05 13:59:18 +0700

    seven commit

--- src/fetchApi/index.jsx
+++ src/fetchApi/index.jsx
@@ -1,29 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getGeoLocation = async (query) => {
   try {
-    const response = await axios.get(
-      `https://nominatim.openstreetmap.org/search?format=json&q=${query}`
-    );
-    const data = await response;
-
-    return data.data;
   } catch (error) {
-    console.error("Error fetching data from API", error);
+    console.error('Error fetching data from API', error);
     return null;
   }
 };
 
 export const getRoute = async (from, to) => {
   try {
-    const response = await axios.get(
-      `https://router.project-osrm.org/route/v1/driving/${from.lon},${from.lat};${to.lon},${to.lat}?overview=full`
-    );
-    const data = await response;
-
-    return data.data;
   } catch (error) {
-    console.error("Error fetching route", error);
+    console.error('Error fetching route', error);
     return null;
   }
 };
